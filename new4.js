f083a013c2cd9ba72999a59745b184583b17ee8d
Abu388 <150917473+Abu388@users.noreply.github.com>
2023-11-28 09:07:04 +0300

    BMI calculator

---
+++ new4.js
@@ -0,0 +1,68 @@
+/* 
+the opening of the object is the only have that
+ name in object it is not possible using of that name;
+function abu (number){
+   return number*number;
+}
+ 
+console.log(abu(5));
+
+****
+
+let isAline =true/false character it have.
+var x=4,age =9;
+if(!(age == x)) it is true if function b/c of negation 
+
+===  it is strictly equals to ,it is the right equals to operator
+
+prompt("what is your mass")
+
+const is useful use const all the time
+
+const mass = prompt("what is your mass")
+const hight =
+use strick
+
+****
+*/
+
+/*const mass = prompt("what is your mass")
+const hight =prompt("what is your hight")
+const bmi = mass/(hight*hight)0
+console.log(bmi);*/
+
+//if function laye ya function true sihone nw yemiserawe.
+
+/*
+function hi(width,hight) {
+   if(width>hight){
+      return true;
+   }
+   else return false;
+}
+console.log(hi(9,3));
+return type sihone console.log laye nw function calling and also assign value yememetareg
+*/
+/*
+   FizzBuzz
+   let x=+prompt('inter the value');
+
+
+*/
+/*
+FizzBuzz('sjak');
+function FizzBuzz(x){
+   if(x%3 ==0 && x%5 ==0){console.log('FizzBuzz'); }
+   else if (x%3==0)console.log('fizz');
+ else if (x%5==0)console.log('Buzz');
+ else if (typeof x != Number)console.log(x);
+ else console.log('not a Number');
+}*/
+/* for of 
+yehe loop yemiserawe le array becha nw engi variable declare aregen ende array yemenaregewe lay 
+bezum tekm yelewem  */
+
+const mass = prompt("what is your mass");
+const hight =prompt("what is your hight");
+const bmi = mass/(hight*hight);
+console.log(bmi);
